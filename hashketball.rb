cf74d7ba457bbd1371783e7b05a35c3dbf76ba4b
Victor Scholz <vgscholz@gmail.com>
2020-07-10 05:04:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,3 +136,12 @@ def num_points_scored(player_search)
   end
 end
 
+def shoe_size_of_player(player_name, game)
+  game.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
+      if player_hash[:name] == player_name
+        return player_hash[:shoe_size]
+      end
+    end
+  end
+end
