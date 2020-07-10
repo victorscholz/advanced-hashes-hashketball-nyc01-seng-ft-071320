22113ffc519dab2365068778f9666d6cf4d9343a
Victor Scholz <vgscholz@gmail.com>
2020-07-10 05:04:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,7 +140,7 @@ def shoe_size_of_player(player_name, game)
   game.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
       if player_hash[:name] == player_name
-        return player_hash[:shoe_size]
+        return player_hash[:shoe]
       end
     end
   end
