a68edbf0ef72cbd946b01b6126be362d78f3d725
Victor Scholz <vgscholz@gmail.com>
2020-07-10 05:01:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,3 +126,9 @@ def game_hash
   }
 end
 
+def num_points_scored(player_search)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:points]
+end
